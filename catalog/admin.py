58165905f22d4374d1e73d7de33c285bcd2becfd
Koval Dmitriy <Criativve13@gmail.com>
2019-03-05 00:18:22 +0200

    add admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -15,6 +15,7 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
+
 admin.site.register(Author, AuthorAdmin)
 
 
@@ -22,12 +23,14 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
